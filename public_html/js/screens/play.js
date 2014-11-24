d075c3999dd0d435e64a810c515ec8dfa48d7135
Jorge Hernandez <jorgeh905187@gmail.com>
2014-11-23 16:03:50 -0800

    added new layers
    added new layers to my level01 map on my mario  website

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -9,7 +9,7 @@ game.PlayScreen = me.ScreenObject.extend({
                 
                 me.levelDirector.loadLevel("level01");
 
-                this.resetPlayer();
+                this.resetPlayer(0, 400);
                 
                 me.input.bindKey(me.input.KEY.RIGHT, "right");
                 me.input.bindKey(me.input.KEY.LEFT, "left");
@@ -28,9 +28,9 @@ game.PlayScreen = me.ScreenObject.extend({
 		me.game.world.removeChild(this.HUD);
 	},
         
-        resetPlayer: function(){
-            var player = me.pool.pull("mario", 0, 420, {});
-            me.game.world.addChild(player, 3);
+        resetPlayer: function(x, y){
+            var player = me.pool.pull("mario", x, y, {});
+            me.game.world.addChild(player, 5);
         }
         
          
