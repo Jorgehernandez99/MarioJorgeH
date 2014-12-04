877541cb834c7aaae3dbc265962c76940b7c4a37
Jorge Hernandez <jorgeh905187@gmail.com>
2014-12-04 11:21:23 -0800

    added a text on my title screen
    added text on my new title screen for my mario game website project

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -4,7 +4,28 @@ game.TitleScreen = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {	
 		var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
-                me.game.world.addChild(titleImage, 1);
+                me.game.world.addChild(titleImage, -10);
+                me.input.bindKey(me.input.KEY.ENTER, "start");
+                
+                me.game.world.addChild(new (me.Renderable.extend({
+                    init: function(){
+                    this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.width]);
+                    this.font = new me.Font("Arial", 46, "white");
+                        },
+                    
+                    draw: function(renderer){
+                        this.font.draw(renderer.getContext(), "Mario", 450, 130);
+                        this.font.draw(renderer.getContext(), "Press Enter to Play", 250, 530);
+                    }
+                    
+                })));
+                
+                this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+                    if(action === "start"){
+                        me.state.change(me.state.PLAY);
+                    }
+                });
+                
 	},
 	
 	
@@ -12,6 +33,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+		me.input.unbindKey(me.input.KEY.ENTER);
+                me.event.unsubscribe(this.handler);
 	}
 });
